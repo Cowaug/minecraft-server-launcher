703ec8e0c08d45731c5b3957cbe4f6bcf597e1f5
Exos Nguyen <esoft288@gmail.com>
2020-01-10 23:59:22 +0700

    20-01-10.23:59

--- src/main/java/com/ebot/mcsl/LaunchUI.java
+++ src/main/java/com/ebot/mcsl/LaunchUI.java
@@ -1,33 +1,75 @@
 package com.ebot.mcsl;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
-public class LaunchUI extends JFXTabPane {
-    public LaunchUI(){
-        Tab mainTab = new Tab("Home");
-        mainTab.setClosable(false);
+import java.util.Arrays;
 
-        HBox saveDir = new HBox(2);
+public class LaunchUI extends JFXTabPane {
+    public LaunchUI() {
+        Tab mainTab = new Tab("Home");
+//        mainTab.setClosable(false);
+        mainTab.setContent(new VBox(getSaveDirBox(),getVersionAndLaunch()));
+        this.getTabs().add(mainTab);
+    }
+
+    private Node[] setHgrow(Node... nodes) {
+        Arrays.asList(nodes).forEach(e -> HBox.setHgrow(e, Priority.ALWAYS));
+        return nodes;
+    }
+
+    private HBox getSaveDirBox() {
+        HBox saveDirBox = new HBox(16);
+
+        Label saveDirLabel = new Label("Save directory");
+        saveDirLabel.setPadding(new Insets(4, 0, 0, 0));
 
         JFXTextField dirField = new JFXTextField(Main.defaultPath);
         dirField.setEditable(false);
-        JFXButton changeDirBtn = new JFXButton("Change");
 
-        saveDir.getChildren().addAll(dirField,changeDirBtn);
+        JFXButton changeSaveDirBtn = new JFXButton("Change");
 
-        Button button = new Button("button1");
-        button.setOnAction(event -> {
-            Tab newTab = new Tab("abc");
+        saveDirBox.getChildren().addAll(setHgrow(saveDirLabel, dirField, changeSaveDirBtn));
+        saveDirBox.setPadding(new Insets(16));
+
+        return saveDirBox;
+    }
+
+    private HBox getVersionAndLaunch() {
+        HBox versionLaunchBox = new HBox(16);
+
+        Label versionLabel = new Label("Server version");
+        versionLabel.setPadding(new Insets(4,0,0,0));
+
+        JFXListView<String> serverList = new JFXListView();
+        //TODO add server from folder, checking jar file and eula
+        serverList.getItems().add("1.15.1");
+        serverList.getItems().add("1.15.2");
+        serverList.getItems().add("1.15.3");
+        serverList.getSelectionModel().select(0);
+
+        JFXButton startBtn = new JFXButton("Start");
+        startBtn.setOnAction(event -> {
+            Tab newTab = new Tab(serverList.getSelectionModel().getSelectedItem());
+            newTab.setClosable(true);
             newTab.setContent(new ServerTab());
             LaunchUI.this.getTabs().add(newTab);
+            LaunchUI.this.getSelectionModel().select(newTab);
         });
 
-        mainTab.setContent(saveDir);
-        this.getTabs().add(mainTab);
+        versionLaunchBox.getChildren().addAll(setHgrow(versionLabel,serverList,startBtn));
+        versionLaunchBox.setPadding(new Insets(16));
+
+        return versionLaunchBox;
     }
 }
--- src/main/java/com/ebot/mcsl/Main.java
+++ src/main/java/com/ebot/mcsl/Main.java
@@ -1,9 +1,17 @@
 package com.ebot.mcsl;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     public static final String defaultPath = System.getenv("APPDATA") + "\\.minecraft";
@@ -11,8 +19,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = new LaunchUI();
+
         primaryStage.setTitle("Minecraft Server Launcher");
         primaryStage.setScene(new Scene(root, 1280, 720));
+//        primaryStage.setResizable(false);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
